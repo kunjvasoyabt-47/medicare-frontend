85ae3325f7b1449fdc0dd9e4da85245f2657c9a9
kunjvasoyabt-47 <kunj.v@bitontree.com>
2026-03-06 16:55:29 +0530

    add package-lock

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.BASE_URL,
   withCredentials: true, // MANDATORY: Allows the browser to send/receive HttpOnly cookies
   headers: {
     'Content-Type': 'application/json',
